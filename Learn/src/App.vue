620bc860677c66bb767bfe31f8ae5ebd711eee6d
acorrea-B <aljyque95@gmail.com>
2021-02-18 20:54:02 -0500

    creacion de componentes

--- Learn/src/App.vue
+++ Learn/src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello maria + lo que sea" />
+    <h1> Primer componenete</h1>
+    <car />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import car from "./components/car.vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+components:{
+  car,
+}
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
